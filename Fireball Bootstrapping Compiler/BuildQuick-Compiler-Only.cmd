3e73b37c4c66186d0a1b4f717db3cc51d1991d73
Kevin "Divo" DiVincenzo <antagonist112358>
2016-01-12 13:35:05 -0600

    Traits partially finished
    - handles resolved parameters
    - handles protected / private method definitions
    - still some issues with "this" parameter to trait method calls

--- Fireball Bootstrapping Compiler/BuildQuick-Compiler-Only.cmd
+++ Fireball Bootstrapping Compiler/BuildQuick-Compiler-Only.cmd
@@ -1,5 +1,5 @@
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 set NoPause=true
-%MSBuild% fbcc.nproj /p:Configuration=Debug /verbosity:n /p:NTargetName=Build /tv:4.0 /p:TargetFrameworkVersion=v4.0
+%MSBuild% Fireball.Compiler.nproj /p:Configuration=Debug /verbosity:n /p:NTargetName=Build /tv:4.0 /p:TargetFrameworkVersion=v4.0
 rem /verbosity:n /p:TargetName=Build
 pause
