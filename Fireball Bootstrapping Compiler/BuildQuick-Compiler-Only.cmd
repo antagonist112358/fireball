9149b789cb5664333136a7ba0f089ea678169d77
Kevin "Divo" DiVincenzo <antagonist112358>
2016-07-22 01:59:44 -0500

    - public / protected vals/vars now generate properties (required for several features) - updated some notes / documentation - fixed issues with traits not properly generating method implementations capable of accessing trait members. - cleaned up parsetree expression serializers (not sure they will be used) - implemented "extends" keyword for class inheritance / trait inclusion - try/catch block now requires each assignment in the try block to be assigned in all catch blocks (null handling) - removing EnvResolvableResolver.n - cleanup on namespace finder - cleanup on partial function typer - added test cases for try/catch assignments in try must be assigned in catch - updated all tests to use "extends" keyword - added new traits test - properties now infer type from assignment expression (so they act like private vars/vals) - cleanup in TypeBuilder.n and TypesManager.n - modified NemerleModifiers enumeration to add implicit flag - future cleanup in MainParser (will eventually split out into multiple expression parsers) - compiler now generates internal names with prefix "_FB_Generated_", instead of "_N_" - Making MergeCompanionModules method in passes.n static * still have issue of macros generating fields properly with initialization expressions - Typer now has flag to keep track of typing method calls vs typing property accessor calls - lots of updates to documentation

--- Fireball Bootstrapping Compiler/BuildQuick-Compiler-Only.cmd
+++ Fireball Bootstrapping Compiler/BuildQuick-Compiler-Only.cmd
@@ -1,5 +1,6 @@
+@echo Starting MS-BUILD for Fireball Compiler...
+@echo ==========================================================================================================
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 set NoPause=true
 %MSBuild% Fireball.Compiler.nproj /p:Configuration=Debug /verbosity:n /p:NTargetName=Build /tv:4.0 /p:TargetFrameworkVersion=v4.0
-rem /verbosity:n /p:TargetName=Build
-pause
+@echo     
