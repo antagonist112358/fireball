ecd0fe3ef40a672f7248803a58fe8a5e1346e452
Kevin "Divo" DiVincenzo <antagonist112358>
2016-07-24 12:05:09 -0500

    - "if ($expression) { $sideEffect : void }" now replaces "when" macro - Added full test suite for "lazy val", both at class level and at method level - Cleanup of macros suite to remove "when" macro usage inside quoted expressions - "instance.Apply(x)" now correctly falls back to default indexer property (called "Item" in CLR) in the event the type does not have an "Apply" method - Fixes to "Fireball.Core.LazyValue[T]"

--- Fireball Bootstrapping Compiler/CompileTest.bat
+++ Fireball Bootstrapping Compiler/CompileTest.bat
@@ -1,3 +1,7 @@
 @echo off
 echo Compiling test: %1
+if [%3]==[lib] goto library
 bin\Debug\fbcc fireball_tests\positive\%1 -o %2
+goto :eof
+:library
+bin\Debug\fbcc fireball_tests\positive\%1 -o %2 -t:Library
